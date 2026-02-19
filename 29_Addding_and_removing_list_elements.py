5a0e037012bfeea6e59ac228adbc80cdc7404393
Ashish Kumar Mahobia <63652300+AshishMahobia@users.noreply.github.com>
2026-02-19 16:28:43 +0530

    first commit

---
+++ 29_Addding_and_removing_list_elements.py
@@ -0,0 +1,13 @@
+lst = [10,20,'Bharath', -10, 30.5]
+# print(lst)
+# print(lst[3])
+# print(lst[3:5])
+# print(lst*4)
+# print(len(lst))
+
+lst.append(40)
+
+lst.remove('Bharath')
+print(lst)
+del(lst[1])
+print(lst)
